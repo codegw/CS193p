34f83e8f88415d098fed4cfd8c59e2e774002d79
codegw <gabrielwilliams205@gmail.com>
2025-12-26 11:48:12 +0000

    Initial Commit

---
+++ CodeBreaker/CodeBreakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodeBreakerApp.swift
+//  CodeBreaker
+//
+//  Created by Gabriel Williams on 26/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct CodeBreakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CodeBreakerTests/CodeBreakerTests.swift
@@ -0,0 +1,17 @@
+//
+//  CodeBreakerTests.swift
+//  CodeBreakerTests
+//
+//  Created by Gabriel Williams on 26/12/2025.
+//
+
+import Testing
+@testable import CodeBreaker
+
+struct CodeBreakerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
