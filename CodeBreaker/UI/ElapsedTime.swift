59834bb237ff0c398d52b0402509598e2627c0d2
codegw <gabrielwilliams205@gmail.com>
2026-01-03 10:57:14 +0000

    Lecture 9 Introduced ElapsedTime() view

---
+++ CodeBreaker/UI/ElapsedTime.swift
@@ -0,0 +1,17 @@
+//
+//  ElapsedTime.swift
+//  CodeBreaker
+//
+//  Created by Gabriel Williams on 03/01/2026.
+//
+
+import SwiftUI
+
+struct ElapsedTime: View {
+    var startTime: Date
+    var endTime: Date?
+    
+    var body: some View {
+        Text("0:00")
+    }
+}
