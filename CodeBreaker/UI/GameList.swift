b3591640df00a5732f1e46c94c0b76543049d485
codegw <gabrielwilliams205@gmail.com>
2026-01-06 13:21:52 +0000

    Lecture 14 Created a FetchDescriptor (which defaults to .true finding all items in CodeBreaker, and in random order) Placed the results within a do-catch block

--- CodeBreaker/UI/GameList.swift
+++ CodeBreaker/UI/GameList.swift
@@ -100,10 +100,16 @@ struct GameList: View {
     }
     
     func addSampleGames() {
-        if games.isEmpty {
-            modelContext.insert(CodeBreaker(name: "Mastermind", pegChoices: [.red, .yellow, .blue, .green]))
-            modelContext.insert(CodeBreaker(name: "Earth Tones", pegChoices: [.orange, .brown, .black, .yellow]))
-            modelContext.insert(CodeBreaker(name: "Undersea", pegChoices: [.blue, .indigo, .cyan]))
+        let fetchDescriptor = FetchDescriptor<CodeBreaker>()
+        do {
+            let results = try modelContext.fetch(fetchDescriptor)
+            if results.count == 0 {
+                modelContext.insert(CodeBreaker(name: "Mastermind", pegChoices: [.red, .yellow, .blue, .green]))
+                modelContext.insert(CodeBreaker(name: "Earth Tones", pegChoices: [.orange, .brown, .black, .yellow]))
+                modelContext.insert(CodeBreaker(name: "Undersea", pegChoices: [.blue, .indigo, .cyan]))
+            }
+        } catch {
+            print("Error: Problem counting games: \(error.localizedDescription)")
         }
     }
 }
