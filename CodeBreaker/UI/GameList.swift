b0c27adbc9e9e935aca0901ca9eeefddbb5b8928
codegw <gabrielwilliams205@gmail.com>
2026-01-06 13:24:49 +0000

    Lecture 14 Replaced with Optional chaining

--- CodeBreaker/UI/GameList.swift
+++ CodeBreaker/UI/GameList.swift
@@ -103,7 +103,7 @@ struct GameList: View {
         let fetchDescriptor = FetchDescriptor<CodeBreaker>()
         
         let results = try? modelContext.fetch(fetchDescriptor)
-        if let results, results.count == 0 {
+        if results?.count == 0 {
             modelContext.insert(CodeBreaker(name: "Mastermind", pegChoices: [.red, .yellow, .blue, .green]))
             modelContext.insert(CodeBreaker(name: "Earth Tones", pegChoices: [.orange, .brown, .black, .yellow]))
             modelContext.insert(CodeBreaker(name: "Undersea", pegChoices: [.blue, .indigo, .cyan]))
